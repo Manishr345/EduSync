d8b086c8c5b95fc8f5da6b6d0072a52619a8f41b
Aryan <aryanbkarki30@gmail.com>
2024-12-30 16:10:42 +0530

    Carousel added and spacing updated

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,8 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
+
 
 const Header = () => {
   const [menuopen, setmenuopen] = useState(false);
   const toggle = () => setmenuopen(!menuopen);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const images = [
+    "https://plus.unsplash.com/premium_photo-1713296255442-e9338f42aad8?q=80&w=1922&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1677567996070-68fa4181775a?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1691962725045-57ff9e77f0bd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1585763465881-62c5d70627bc?q=80&w=1978&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1527891751199-7225231a68dd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
+  const nextSlide = () => setCurrentSlide((prev) => (prev + 1) % images.length);
+  const prevSlide = () => setCurrentSlide((prev) => (prev - 1 + images.length) % images.length);
+  useEffect(() => {
+    const intervalId = setInterval(nextSlide, 3000); 
+    return () => clearInterval(intervalId);
+  }, []);
+
+
   return (
     /* Don't delete this comment work in progress
       <header className='bg-black text-white p-10 flex justify-between text-3xl'>
@@ -10,67 +27,123 @@ const Header = () => {
       <div className='hover:bg-slate-600 rounded-lg'><button className='w-11 h-11'><i className="fa-solid fa-bars"></i></button></div>
       </header>  
     */
-    <nav className="bg-black lg:h-24 lg:p-4 xl:h-32 xl:p-7 min-[768px]:max-[1023px]:w-[100%]">
-      <div className="max-w-screen flex flex-wrap items-center justify-between p-4 min-[768px]:max-[1023px]:flex gap-5 xl:flex xl:justify-between">
-        <a href="https://flowbite.com/" className="flex gap-1">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="xl:h-10 h-7" alt="Flowbite Logo" />
-          <span className="text-xl font-semibold dark:text-white xl:text-4xl">EduSync</span>
-        </a>
+    <>
+      <nav className="bg-black lg:h-24 lg:p-4 xl:h-32 xl:p-7 min-[768px]:max-[1023px]:w-[100%]">
+        <div className="max-w-screen flex flex-wrap items-center justify-between p-4 min-[768px]:max-[1023px]:flex gap-5 xl:flex xl:justify-between">
+          <a href="#" className="flex gap-1">
+            <img src="https://flowbite.com/docs/images/logo.svg" className="xl:h-10 h-7" alt="Flowbite Logo" />
+            <span className="text-xl font-semibold dark:text-white xl:text-4xl">EduSync</span>
+          </a>
+          <button
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-xl text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded={menuopen ? "true" : "false"}
+            onClick={toggle}
+          >
+            <i className="fa-solid fa-bars"></i>
+          </button>
+          <div className={`w-full md:block md:w-auto ${menuopen ? "" : "hidden"}`} id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-14 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 min-[768px]:max-[1023px]:space-x-8 lg:space-x-8 xl:space-x-14">
+              <li>
+                <a href="#" className="xl:text-xl block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  Activity
+                </a>
+              </li>
+              <li>
+                <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  Achievements
+                </a>
+              </li>
+              <li>
+                <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  Courses
+                </a>
+              </li>
+              <li>
+                <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  Examination
+                </a>
+              </li>
+              <li>
+                <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  Admission
+                </a>
+              </li>
+              <li>
+                <a href="/profile" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  Profile
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="relative w-full mt-2 mb-5 pl-5 pr-5">
+        <div className="relative h-[350px] overflow-hidden sm:h-[500px] md:h-[600px] xl:h-[770px] rounded-3xl">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={`brightness-50 absolute w-full h-full transition-opacity duration-700 ease-in-out ${index === currentSlide ? "opacity-100" : "opacity-0"
+                }`}
+            >
+              <img src={image} className="w-full h-full object-cover" alt={`Slide ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+
+        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+          {images.map((_, index) => (
+            <button
+              key={index}
+              type="button"
+              className={`w-2 h-2 rounded-full ${index === currentSlide ? "bg-[#0d3645]" : "bg-white"
+                }`}
+              aria-current={index === currentSlide ? "true" : "false"}
+              onClick={() => setCurrentSlide(index)}
+            ></button>
+          ))}
+        </div>
+
         <button
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-xl text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded={menuopen ? "true" : "false"}
-          onClick={toggle}
+          className="absolute top-0 start-5 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          onClick={prevSlide}
         >
-          <i className="fa-solid fa-bars"></i>
+          <span className="inline-flex items-center justify-center w-7 h-7 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-2 group-focus:ring-white">
+            <svg className="w-2 h-2 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+            </svg>
+            <span className="sr-only">Previous</span>
+          </span>
         </button>
-        <div className={`w-full md:block md:w-auto ${menuopen ? "" : "hidden"}`} id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-14 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 min-[768px]:max-[1023px]:space-x-8 lg:space-x-8 xl:space-x-14">
-            <li>
-              <a href="#" className="xl:text-xl block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                Activity
-              </a>
-            </li>
-            <li>
-              <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                Achievements
-              </a>
-            </li>
-            <li>
-              <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                Courses
-              </a>
-            </li>
-            <li>
-              <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                Examination
-              </a>
-            </li>
-            <li>
-              <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                Admission
-              </a>
-            </li>
-            <li>
-              <a href="#" className="xl:text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                Profile
-              </a>
-            </li>
-          </ul>
-        </div>
+
+        <button
+          type="button"
+          className="absolute top-0 end-5 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          onClick={nextSlide}
+        >
+          <span className="inline-flex items-center justify-center w-7 h-7 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-2 group-focus:ring-white">
+            <svg className="w-2 h-2 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+            </svg>
+            <span className="sr-only">Next</span>
+          </span>
+        </button>
       </div>
-    </nav>
+
+    </>
   )
 }
 export default Header
--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -2,69 +2,81 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <><div className='w-full flex justify-center items-center bg-black'>
-            <div class="relative py-3 sm:max-w-xl sm:mx-auto w-full">
-                <div
-                    class="relative px-4 py-10 bg-black mx-8 md:mx-0  rounded-3xl sm:p-10 border  shadow-none transition-all duration-500 cursor-pointer   hover:shadow-[-6px_-6px_15px_#0ea5e9] "
-                >
-                    <div class="max-w-md mx-auto text-white">
-                        <div class="flex items-center space-x-5 justify-center">
-                            <h1 class='text-2xl font-bold'>Login</h1>
-                        </div>
-                        <div class="mt-5">
-                            <label
-                                for="login"
-                                class="font-semibold text-sm text-gray-400 pb-1 block"
-                            >E-mail</label
-                            >
-                            <input
-                                id="login"
-                                type="text"
-                                class="border-2 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
-                            />
-                            <label
-                                for="password"
-                                class="font-semibold text-sm text-gray-400 pb-1 block"
-                            >Password</label
-                            >
-                            <input
-                                id="password"
-                                type="password"
-                                class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
-                            />
-                        </div>
-                        <div class="text-right mb-4">
-                            <a
-                                href="#"
-                                class="text-xs font-display font-semibold text-gray-500 hover:text-gray-400 cursor-pointer"
-                            >
-                                Forgot Password?
-                            </a>
-                        </div>
-                        <div class="mt-5">
-                            <button
-                                type="submit"
-                                class="py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
-                            >
-                                Log in
-                            </button>
-                        </div>
-                        <div class="flex items-center justify-between mt-4">
-                            <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
-                            <a
-                                href="#"
-                                class="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
-                            >or sign up</a
-                            >
-                            <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+        <>
+            <div className='w-full h-[100vh] flex justify-center items-center bg-black'>
+                <div className="relative py-3 sm:max-w-xl sm:mx-auto w-full">
+                    <div
+                        className="relative px-4 py-10 bg-[#0d3645ba] mx-8 md:mx-0  rounded-3xl sm:p-10 border  shadow-none transition-all duration-500 cursor-pointer   hover:shadow-[-6px_-6px_15px_#0ea5e9] "
+                    >
+                        <div className="max-w-md mx-auto text-white">
+                            <div className="flex items-center space-x-5 justify-center">
+                                <h1 className='text-2xl font-bold'>Login</h1>
+                            </div>
+                            <div className="mt-5">
+                                <label
+                                    for="login"
+                                    className="font-semibold text-sm text-gray-400 pb-1 block"
+                                >UID</label
+                                >
+                                <input
+                                    id="login"
+                                    type="text"
+                                    required
+                                    class="border-2 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
+                                />
+                                <label
+                                    for="login"
+                                    class="font-semibold text-sm text-gray-400 pb-1 block"
+                                >Username</label
+                                >
+                                <input
+                                    id="login"
+                                    type="text"
+                                    required
+                                    class="border-2 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
+                                />
+                                <label
+                                    for="login"
+                                    class="font-semibold text-sm text-gray-400 pb-1 block"
+                                >E-mail</label
+                                >
+                                <input
+                                    id="login"
+                                    type="email"
+                                    class="border-2 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
+                                />
+                                <label
+                                    for="password"
+                                    class="font-semibold text-sm text-gray-400 pb-1 block"
+                                >Password</label
+                                >
+                                <input
+                                    id="password"
+                                    type="password"
+                                    class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
+                                />
+                            </div>
+                            <div class="mt-5">
+                                <button
+                                    type="submit"
+                                    class="py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+                                >
+                                    Log in
+                                </button>
+                            </div>
+                            <div class="flex items-center justify-between mt-4">
+                                <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+                                <a
+                                    href="#"
+                                    class="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
+                                >or sign up</a
+                                >
+                                <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-            
-
-
         </>
     )
 }
