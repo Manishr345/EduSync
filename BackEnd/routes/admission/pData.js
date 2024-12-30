0c45b7cebe56b54b1add620a216f3261a2a38034
Manishr345 <manishr030405@gmail.com>
2024-12-30 21:35:14 +0530

    Merge conflict resolved in frontend

--- BackEnd/routes/admission/pData.js
+++ BackEnd/routes/admission/pData.js
@@ -2,21 +2,21 @@ const express = require('express');
 const router = express.Router();
 const PersonalDetail = require('../../models/admission/personalDetails');
 
-router.post('/personaldetails',async (req,res)=>{
+router.post('/personaldetails', async (req, res) => {
     const pd = await PersonalDetail.create({
-        fullName : req.body.fullName,
-        dob : req.body.dob ,
-        gender: req.body.gender ,
-        nationality : req.body.nationality ,
-        contact : req.body.contact,
-        email : req.body.email ,
-        address : req.body.address,
-        parentName : req.body.parentName ,
-        relation : req.body.relation,
-        parentContact : req.body.parentContact,
-        parentEmail : req.body.parentEmail,
+        fullName: req.body.fullName,
+        dob: req.body.dob,
+        gender: req.body.gender,
+        nationality: req.body.nationality,
+        contact: req.body.contact,
+        email: req.body.email,
+        address: req.body.address,
+        parentName: req.body.parentName,
+        relation: req.body.relation,
+        parentContact: req.body.parentContact,
+        parentEmail: req.body.parentEmail,
         occupation: req.body.occupation
     })
     res.send(pd)
 })
-module.exports =router;
+module.exports = router;
