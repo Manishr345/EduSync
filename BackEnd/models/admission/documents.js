005710930d3318f61fbc5a2e4fcb4631ac3037fb
Manishr345 <manishr030405@gmail.com>
2025-01-02 20:36:13 +0530

    Created document model

---
+++ BackEnd/models/admission/documents.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const documentSchema = mongoose.Schema({
+    passportSizePhoto: {
+        type: String,
+        required: true
+    },
+    marksheet10th: {
+        type: String,
+        required: true
+    },
+    marksheet12th: {
+        type: String,
+        required: true
+    },
+    certificate10th: {
+        type: String,
+        required: true
+    },
+    certificate12th: {
+        type: String,
+        required: true
+    },
+    indentityProof: {
+        type: String,
+        required: true
+    },
+    birthCertificate: {
+        type: String,
+        required: true
+    },
+    addressProof: {
+        type: String,
+        required: true
+    }
+})
+
+export default mongoose.model('documents', documentSchema);
