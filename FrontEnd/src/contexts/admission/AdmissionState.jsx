ecd7bf269e04ca32025892963ca8facaba7c5987
Manishr345 <manishr030405@gmail.com>
2025-01-02 19:42:12 +0530

    Connected educational details with backend

--- FrontEnd/src/contexts/admission/AdmissionState.jsx
+++ FrontEnd/src/contexts/admission/AdmissionState.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import AdmissionContext from "./AdmissionContext";
 
 const AdmissionState = (props) => {
+    const [id, setId] = useState(() => {
+        return localStorage.getItem('studentid') || '';
+    })
     const [personal, setPersonal] = useState({});
     const personalDetails = async (fullName, dob, gender, nationality, contact, email, address, parentName, relation, parentContact, parentEmail, occupation) => {
         const response = await fetch('http://localhost:5000/admission/personaldetails', {
@@ -12,10 +15,25 @@ const AdmissionState = (props) => {
             body: JSON.stringify({fullName, dob, gender, nationality, contact, email, address, parentName, relation, parentContact, parentEmail, occupation})
         })
         const json = await response.json();
+        if(json._id){
+            setId(json._id); // Update state
+            localStorage.setItem('studentid', json._id);
+        }
+    }
+    const educationalDetails = async (schoolName, collegeName, schoolGrade, collegeGrade, highestQualification) => {
+        const studentId = localStorage.getItem('studentid');
+        const response = await fetch(`http://localhost:5000/admission/educationaldetails/${studentId}`, {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({schoolName, collegeName, schoolGrade, collegeGrade, highestQualification})
+        })
+        const json = await response.json();
         console.log(json);
     }
     return (
-        <AdmissionContext.Provider value={{personal, setPersonal, personalDetails}}>
+        <AdmissionContext.Provider value={{personal, setPersonal, personalDetails, educationalDetails}}>
             {props.children}
         </AdmissionContext.Provider>
     )
