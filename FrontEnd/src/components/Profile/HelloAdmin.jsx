ded23e9ae2c9fd5822fbd18092393d1f63469809
Aryan <aryanbkarki30@gmail.com>
2025-01-03 19:12:44 +0530

    File placed

--- FrontEnd/src/components/Profile/HelloAdmin.jsx
+++ FrontEnd/src/components/Profile/HelloAdmin.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import React from 'react'
-import AdminContext from '../contexts/admin/AdminContext'
+import AdminContext from '../../contexts/admin/AdminContext'
 import { useContext } from 'react'
 
 const HelloAdmin = () => {
