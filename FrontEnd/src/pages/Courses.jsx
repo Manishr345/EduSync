f743a4c4c9331f8c0bddfb96b1406d6e6ea1999b
DivyaBurathoki16 <divyaburathoki16@gmail.com>
2025-01-14 19:20:51 +0530

    Updated Course page(mehrbaani)

--- FrontEnd/src/pages/Courses.jsx
+++ FrontEnd/src/pages/Courses.jsx
@@ -73,7 +73,7 @@ const Courses = () => {
       <Header />
       <div className="p-10 bg-black text-white">
         <div className="container mx-auto">
-          <h1 className="text-6xl font-bold text-center mb-10">Our Courses</h1>
+        <h1 className="text-4xl md:text-6xl font-bold text-center mb-10">Our Courses</h1>
           <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
             {courses.map((course, index) => (
               <div
