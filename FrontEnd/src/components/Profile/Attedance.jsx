b83d34a461930755aa98c932650c3fded8675e81
Ankush pal <155830977+ankush9769@users.noreply.github.com>
2025-01-15 12:04:16 +0530

    attendance page created

---
+++ FrontEnd/src/components/Profile/Attedance.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Attedance = () => {
+  return (
+    <div className='w-[90vw] bg-gray-800 flex flex-col justify-center items-center'>
+        <div className="container flex w-full bg-blue-950">
+            <p>sub1</p>
+            <p>present</p>
+            <p>upsent</p>
+            <form action="">
+                <input type="file" name="docs" placeholder="name"/>
+            </form>
+        </div>
+        <div className="container flex w-full bg-blue-950">
+            <p>sub2</p>
+            <p>present</p>
+            <p>upsent</p>
+            <form action="">
+                <input type="file" name="docs" placeholder="name"/>
+            </form>
+        </div>
+        <div className="container flex w-full bg-blue-950">
+            <p>sub3</p>
+            <p>present</p>
+            <p>upsent</p>
+            <form action="">
+                <input type="file" name="docs" placeholder="name"/>
+            </form>
+        </div>
+   
+    </div>
+  )
+}
+
+export default Attedance
